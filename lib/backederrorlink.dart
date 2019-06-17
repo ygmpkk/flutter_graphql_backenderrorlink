d75c884cae7475539b4e8b44d85172708587d461
Timothy <ygmpkk@gmail.com>
2019-06-17 11:43:50 +0800

    add backend error link for graphql_flutter

---
+++ lib/backederrorlink.dart
@@ -0,0 +1,74 @@
+library backederrorlink;
+
+import 'dart:async';
+
+import 'package:http/http.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:graphql/internal.dart';
+
+typedef Callback = Function(FetchResult, StreamedResponse);
+
+class BackendErrorLink extends Link {
+  BackendErrorLink({
+    this.onClientError,
+    this.onUnauthorized,
+    this.onForbidden,
+    this.onNotFound,
+    this.onServerError,
+    this.onUnknownError,
+  }) : super(
+          request: (Operation operation, [NextLink forward]) {
+            StreamController<FetchResult> controller;
+
+            Future<void> onListen() async {
+              await controller
+                  .addStream(forward(operation).map((FetchResult result) {
+                if (result != null && result.errors != null) {
+                  StreamedResponse response =
+                      operation.getContext()["response"];
+                  if (response != null) {
+                    switch (response.statusCode) {
+                      case 400:
+                        onClientError(result, response);
+                        break;
+                      case 401:
+                        onUnauthorized(result, response);
+                        break;
+                      case 403:
+                        onForbidden(result, response);
+                        break;
+                      case 404:
+                        onNotFound(result, response);
+                        break;
+                      case 500:
+                      case 501:
+                      case 502:
+                      case 503:
+                        onClientError(result, response);
+                        break;
+                      default:
+                        onUnknownError(result, response);
+                        break;
+                    }
+                  }
+                }
+
+                return result;
+              }));
+
+              await controller.close();
+            }
+
+            controller = StreamController<FetchResult>(onListen: onListen);
+
+            return controller.stream;
+          },
+        );
+
+  Callback onClientError;
+  Callback onUnauthorized;
+  Callback onForbidden;
+  Callback onNotFound;
+  Callback onServerError;
+  Callback onUnknownError;
+}
---
+++ test/backederrorlink_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:backederrorlink/backederrorlink.dart';
+import 'package:graphql/internal.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:http/http.dart';
+
+void main() {
+  test('test callback', () async {
+    final BackendErrorLink backendErrorLink = BackendErrorLink(
+        onUnauthorized: (FetchResult result, StreamedResponse response) {
+      expect(new Future.value(response.statusCode), completion(equals(401)));
+    });
+
+    final HttpLink httpLink = HttpLink(
+        uri:
+            'https://requestloggerbin.herokuapp.com/bin/2c0b451d-b679-4d5b-8687-0d891bead416/view');
+
+    final Link link = backendErrorLink.concat(httpLink as Link);
+
+    GraphQLClient graphQLClient = GraphQLClient(
+      cache: OptimisticCache(
+        dataIdFromObject: typenameDataIdFromObject,
+      ),
+      link: link,
+    );
+
+    await graphQLClient.query(QueryOptions(
+      document: '''
+        {
+          user {
+            user
+          }
+        }
+      ''',
+    ));
+  });
+}
